5fe31625407fe9b50298664ffd62e5cd8ee5f904
George Higbie <ghigbie@monigle.com>
2019-07-28 15:58:35 -0600

    Add views

--- products/views.py
+++ products/views.py
@@ -4,3 +4,11 @@ from django.views.generic.list import ListView
 from .models import Product, Manufacturer
 
 
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html"
