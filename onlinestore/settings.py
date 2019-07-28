770a74289bc72480d2c4e648bb07433360a2ed81
George Higbie <ghigbie@monigle.com>
2019-07-28 16:26:48 -0600

    Set template location and make migration

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'onlinestore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'onlinestore/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
