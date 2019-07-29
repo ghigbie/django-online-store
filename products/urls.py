ad2880fca5014d26f5b9a40e8fc1153b7987c285
ghigbie <gehigbie@gmail.com>
2019-07-28 20:20:24 -0600

    Edit urls

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import product_list
 #from .views import ProductDetailView, ProductListView
 
 urlpatterns = [
-    path("products/"),product_list, name="product-list"),
+    path("products/", product_list, name="product-list"),
 ]
