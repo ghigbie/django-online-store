f5c784bbd1c9fb34ba0a828b13b1b73fad4930a7
ghigbie <gehigbie@gmail.com>
2019-07-28 20:10:37 -0600

    Add products endpoint

--- products/views.py
+++ products/views.py
@@ -1,14 +1,24 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
+from django.http import JsonResponse
 
 from .models import Product, Manufacturer
 
+def product_list(request):
+    products = Product.objects.all()
+    data = {
+        "products": list(products.values("pk", "name"))
+    }
+    response = JsonResponse(data)
+    return response
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
+
+#BELOW IS FOR STANDARD DJANGO PRODUCT
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = "products/product_detail.html"
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = "products/product_list.html"
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = "products/product_list.html"
